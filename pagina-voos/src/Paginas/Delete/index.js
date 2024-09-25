378f8b0ccffa73085b1dfc6c7f4d5892999b99b3
Enzo-dorta <91228736+Enzo-dorta@users.noreply.github.com>
2024-09-25 19:25:26 +0000

    Co-authored-by: Breno Saraiva <BrenoSaraiva-exe@users.noreply.github.com>
    atualizando tela de delete e criação pasta atualizar

--- pagina-voos/src/Paginas/Delete/index.js
+++ pagina-voos/src/Paginas/Delete/index.js
@@ -4,27 +4,29 @@ import NavBar from "../../Components/NavBar/index.jsx"
 
 
 
-class Delete extends React.Components{
+class Delete extends React.Component{
 
 
-    const Menu = style.form`
-        width: 400px;
-        height: 400px;
-        align-content: center;
-        display: flex;
-        flex-direction: colunm;
-        `
-    const Button = style.button`
-        color: #BF4F74;
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em 1em;
-        border: 2px solid #BF4F74;
-        border-radius: 3px;
-    `
+    // const Menu = style.form`
+    //     width: 400px;
+    //     height: 400px;
+    //     align-content: center;
+    //     display: flex;
+    //     flex-direction: colunm;
+    //     `
+    // const Button = style.button`
+    //     color: #BF4F74;
+    //     font-size: 1em;
+    //     margin: 1em;
+    //     padding: 0.25em 1em;
+    //     border: 2px solid #BF4F74;
+    //     border-radius: 3px;
+    // `
     render(){
         return(
-            <NavBar/>
+            <>
+            <NavBar />
+            <Menu />
             </>
         )
 
